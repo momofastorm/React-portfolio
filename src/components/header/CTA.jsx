1ee2edd2bc26e9fce0c54833afe1072261e16a41
momofastorm <marcyrappaport.blessed@gmail.com>
2023-08-29 16:28:56 -0500

    updated buttons

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import resume from '../../assets/resume.pdf'
+const CTA = () => {
+  return (
+    <div className='cta'>
+        <a href={resume} download className='btn'>Download Resume</a>
+        <a href="#contact" className='btn btn-primary'>Contact</a>
+    </div>
+  )
+}
+
+export default CTA
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,18 @@
+/* eslint-disable jsx-a11y/heading-has-content */
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const Header = () => {
     return (
-        <div>Header</div>
+       <header>
+        <div className="container header__container">
+            <h5>Hello I am</h5>
+            <h1>Marcy Rappaport</h1>
+            <h5 className="text-light">Fullstack Technical Trainer</h5>
+            <CTA />
+        </div>
+        </header>
     )
 }
 
